526852aa195caa78612acc1f83b1c8beaa8a4d3d
gzavaglia <gio.zavaglia@gmail.com>
2019-12-16 20:19:08 -0600

    created single contact class

---
+++ js-frontend/src/components/contact.js
@@ -0,0 +1,8 @@
+class Contact {
+    constructor(contactJSON){
+        this.id = contactJSON.id
+        this.name = contactJSON.name
+        this.phone = contactJSON.phone
+        this.email = contactJSON.email
+    }
+}
