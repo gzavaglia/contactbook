d0f5e3a44b72c7c998f875f525e7b36287c251e4
gzavaglia <gio.zavaglia@gmail.com>
2019-12-16 19:54:13 -0600

    database was seeded

--- contacts-api/db/seeds.rb
+++ contacts-api/db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create(
+    [
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email},
+        {name: Faker::TvShows::RuPaul.queen, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email}
+    ]
+)
+
