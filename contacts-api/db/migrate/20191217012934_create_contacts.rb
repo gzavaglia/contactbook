25a04866c23b658a872c943efe27917ddef542c8
gzavaglia <gio.zavaglia@gmail.com>
2019-12-16 19:33:08 -0600

    created migration

--- contacts-api/db/migrate/20191217012934_create_contacts.rb
+++ contacts-api/db/migrate/20191217012934_create_contacts.rb
@@ -1,7 +1,9 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-
+      t.string :name
+      t.string :phone
+      t.string :email
       t.timestamps
     end
   end
